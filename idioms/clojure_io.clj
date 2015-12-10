02c2057eec4b7fd2932a4965e00b631ad8473984
marchdown <marchdown@gmail.com>
2015-12-10 22:11:41 +0300

    building blocks
    for solving practice problems quickly

--- idioms/clojure_io.clj
+++ idioms/clojure_io.clj
@@ -1,39 +1,35 @@
 ;; TODO: namespace declarations go here.
 ;; HWÆT how do I import this file from a repl?
 (defn read-int [] (Integer. (read-line)))
-
-
+;(def N (read-int))
 (defn words [s] (clojure.string/split s #"\s"))
 (defn read-line-of-ints [] (map #(Integer. %) (words (read-line))))
-(defn print-seq [s] (apply map println s))
+;(def xs (read-line-of-ints))
+
+(defn announce [result]
+  (print (apply str (interpose " " result)))) ;; <-- does this actually work?
+
+(defn print-seq [s] (println (apply str (interpose " " s)))) ; TODO: get rid of the trailing space
 
 ;;;; reasonable problem structure:
 ;(def N (read-int))
 ;(def xs (read-line-of-ints))
 ;
 ;(def result (solve xs))
-;(def answer (format result))
-;(print answer)
+;(announce result)
 
-(defn solve [xs p] (partition xs (count xs) p))
-(defn partition [ar n p]
-  (concat (filter (partial > p) xs) (filter (partial <= p) xs))
-  )
-
-(defn format-result [result]
-  (drop-last (str (map #(format "%d " %) result))))
-                                        ; this is a lazy functional solution which probably takes forever
 ; HWÆT learn how to measure running time of a function.
 
 ;; how to force evaluation so that I don't get a lazy seq of side-effect-ful exprs? 
+;; doseq and dotimes are strict and are intended to be used for side-effects
 
 ;; N.B. ns is a bad variable name in clojure, because by convention it means
 ;; namespace, not seq-of-nats
 
 
-;; clojure construct that don't quite make sense to me yet:
+;; HWÆT clojure construct that don't quite make sense to me yet:
 ;; for
-;; proc
+;; do, doseq, dotimes — how do we force evaluation exactly?
 ;; loop
 ;; apply str idiom
 
